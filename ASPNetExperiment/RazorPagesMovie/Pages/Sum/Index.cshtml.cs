fd830927bb64a4dc23c6966b5592f3d92579f363
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2020-05-04 10:05:59 +0530

    changes

---
+++ ASPNetExperiment/RazorPagesMovie/Pages/Sum/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public Sum Sum { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
