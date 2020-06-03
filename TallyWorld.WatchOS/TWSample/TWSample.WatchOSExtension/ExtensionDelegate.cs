46a447e8f976d3a5d74fd2f55d7a97f93275e4fc
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2020-06-03 10:58:00 +0530

    WatchOS

---
+++ TallyWorld.WatchOS/TWSample/TWSample.WatchOSExtension/ExtensionDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Foundation;
+using WatchKit;
+
+namespace TWSample.WatchOSExtension
+{
+    public class ExtensionDelegate : WKExtensionDelegate
+    {
+        public ExtensionDelegate()
+        {
+        }
+    }
+}
+
