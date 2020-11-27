a335080f296429b2f8b91fbc1cb02eba5a25ac89
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2020-11-27 18:38:51 +0000

    Updated Program.cs

--- TestRepo/TestConsoleApp/TestConsoleApp/Program.cs
+++ TestRepo/TestConsoleApp/TestConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Change from tuhin1");
+            Console.WriteLine("Change from other machine");
         }
     }
 }
