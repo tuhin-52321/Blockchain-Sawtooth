86bf5688492f9237ffe9bc1f18b2852f8977423c
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2020-11-27 18:44:42 +0000

    Updated Program.cs

--- TestRepo/TestConsoleApp/TestConsoleApp/Program.cs
+++ TestRepo/TestConsoleApp/TestConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Change from other machine");
+            Console.WriteLine("Change from Remote");
         }
     }
 }
