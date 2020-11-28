89a094afaf205be5c6eff2ae9e191c214b370321
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2020-11-28 16:42:46 +0000

    Updated Program.cs

--- TestRepo/TestConsoleApp/TestConsoleApp/Program.cs
+++ TestRepo/TestConsoleApp/TestConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Change from remote");
+            Console.WriteLine("Change from remote - another change");
             Console.WriteLine("Added line 2 in local");
 
         }
