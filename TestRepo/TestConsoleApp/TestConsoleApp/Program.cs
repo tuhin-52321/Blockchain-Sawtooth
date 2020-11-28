950e90a9b13f4e9460b84768213adf66ee002093
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2020-11-28 20:35:44 +0530

    another test for bug

--- TestRepo/TestConsoleApp/TestConsoleApp/Program.cs
+++ TestRepo/TestConsoleApp/TestConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Change from local - another");
+            Console.WriteLine("Change from local - another test");
         }
     }
 }
