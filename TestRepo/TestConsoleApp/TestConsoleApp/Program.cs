372b26e8ac08e6afe6400c3d0a3241cf33c17766
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2020-11-29 10:32:57 +0000

    Updated Program.cs

--- TestRepo/TestConsoleApp/TestConsoleApp/Program.cs
+++ TestRepo/TestConsoleApp/TestConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
+            Console.WriteLine("Hello Harsha");
         }
     }
 }
