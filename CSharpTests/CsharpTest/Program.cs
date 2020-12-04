1771b05c3d2779b38a8efa7a27aedb7c349128b1
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2020-12-05 00:18:41 +0530

    New test codes

---
+++ CSharpTests/CsharpTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CsharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IDictionary<string, string> table = new Dictionary<string, string>();
+
+            table["KEY"] =  "v1";
+
+            table["Key"] = "v2";
+
+            table["KEY"] = "v3";
+
+
+            Console.WriteLine(table["KEY"]);
+
+            Console.WriteLine(table["Key"]);
+
+            //Console.WriteLine(table["None"]);
+
+
+        }
+    }
+}
