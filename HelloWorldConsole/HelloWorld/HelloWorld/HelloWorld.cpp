31fed27e2cac05708d041c18f35ede07e423ee4f
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2020-12-10 17:37:16 +0530

    7.8 screenshot

--- HelloWorldConsole/HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorldConsole/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World! v 7.8 test\n";
+    std::cout << "Hello World! v 7.8 test screenshot\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
