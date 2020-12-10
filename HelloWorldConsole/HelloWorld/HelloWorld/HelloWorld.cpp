501daca7f9165ee25e6a927f03a4143abed7f627
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2020-12-10 18:31:14 +0530

    mulit commit test

--- HelloWorldConsole/HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorldConsole/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World! v 7.8 test screenshot\n";
+    std::cout << "Hello World! v 7.8 test screenshot - multi commit scenario\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
